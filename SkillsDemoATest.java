b927aa09eb60ef21de027ab0c4a8ce45ff363da3
cindy4127 <103284173+cindy4127@users.noreply.github.com>
2022-05-11 23:44:52 -0700

    Update SkillsDemoATest.java

--- SkillsDemoATest.java
+++ SkillsDemoATest.java
@@ -3,7 +3,7 @@ import org.junit.*;
 
 public class SkillsDemoATest {
     // this is a test file for the factorial function
-    @test
+    @Test
     public void factorial(){
         assertEquals(720, SkillsDemoA.factorial(8)); //fails initally
     }
