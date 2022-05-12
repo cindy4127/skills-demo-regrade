ff429665b6d69460543ade38af2b5fc0ba6a9b1c
cindy4127 <103284173+cindy4127@users.noreply.github.com>
2022-05-11 23:52:51 -0700

    Update SkillsDemoA.java

--- SkillsDemoA.java
+++ SkillsDemoA.java
@@ -1,10 +1,9 @@
 public class SkillsDemoA {
     public static int factorial(int n){
         //factorial of zero is one
-        if (n==0){
+        if (n == 0){
             return 1;
         }
-        return n * (n-1);
+        return n * factorial(n-1);
     }
-
 }
